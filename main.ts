169bb4562cc4362d138939aa557eeb1c5851956a
Bastian Hodapp <bastianhodapp@gmail.com>
2022-06-26 16:21:35 +0200

    add first db query

--- main.ts
+++ main.ts
@@ -1,6 +1,24 @@
-import mysql from "mysql";
-import * as dotenv from 'dotenv';
+import mysql, { Connection } from "mysql";
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
 console.log(process.env.DB_HOST);
+
+const connection: Connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || "3306"),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("Connected!");
+
+    connection.query("SELECT * FROM users", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+    });
+});
